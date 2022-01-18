8d75c11618e12b0e32070bd54b2a5e60937da4f7
Dulara Fernando(M00727964) <hf221@live.mdx.ac.uk>
2022-01-18 14:02:31 +0000

    comments

--- examples/homework.py
+++ examples/homework.py
@@ -8,7 +8,7 @@ client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 client.put_pixels(leds)
 
-for led in range(60):
+for led in range(60): #pick out indeces: led = 0,1,2,3...
     leds[led] = (255,0,0)
     time.sleep(.1)
     client.put_pixels(leds)
@@ -19,13 +19,13 @@ while led<60:
     leds[59-led] = (0,255,0)
     time.sleep(.1)
     client.put_pixels(leds)
-    led = led + 1
+    led = led + 1 #or reverse if you want
 
 led=0
-while led<60:
-    for rows in range(3):
+while led<60: #scroll all rows at the same time
+    for rows in range(3): # 1st 3 lines to left from right
         leds[led+rows*60] = (0,0,255)
-    for rows in range(3,6):
+    for rows in range(3,6): #last3 lines to right from left
         leds[59-led+rows*60] = (100,100,255)
     client.put_pixels(leds)
     time.sleep(.1)
